0c2d9fa2949b897200b370a6d4193cfdaf9c56c2
VeritasAmo <linhe.xu@hotmail.com>
2017-01-10 13:16:11 -0500

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""This Program asks for a user name input and output a greeting"""
+
+def main():
+	name = input("Quid est nomen tibi? ")
+	print ("Bene %s!" % name)
+
+main()
